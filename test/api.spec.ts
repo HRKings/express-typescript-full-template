7f7b20bd199ef0f76da0bb09afc030830b6faf7b
Helton Reis <hrkings@rhaizzing.com>
2021-10-26 23:30:43 -0300

    [refactor] Organize tests

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -1,26 +1,31 @@
+import { describe, it } from 'mocha';
 import request from 'supertest';
 
 import server from '../src/Server.js';
 
-describe('GET /api/v1', () => {
-  it('responds with a json message', (done) => {
-    request(server)
-      .get('/api/v1')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: '👋🌎🌍🌏 - Hello World from the API!',
-      }, done);
+describe('Sample Controller', () => {
+  describe('GET /api/v1', () => {
+    it('responds with a json message', (done) => {
+      request(server)
+        .get('/api/v1')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          message: '👋🌎🌍🌏 - Hello World from the API!',
+        }, done);
+    });
   });
-});
 
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', (done) => {
-    request(server)
-      .get('/api/v1/emojis')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, ['🐹', '⚡', '🔌'], done);
+  describe('GET /api/v1/emojis', () => {
+    it('responds with a json message', (done) => {
+      request(server)
+        .get('/api/v1/emojis')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, ['🐹', '⚡', '🔌'], done);
+    });
+  });
+
   describe('POST /api/v1/name', () => {
     it('should respond with a JSON message containing the name', (done) => {
       request(server)
--- test/server.spec.ts
+++ test/server.spec.ts
@@ -1,25 +1,28 @@
+import { describe, it } from 'mocha';
 import request from 'supertest';
 
 import server from '../src/Server.js';
 
-describe('app', () => {
-  it('should respond with a not found message', (done) => {
-    request(server)
-      .get('/this-does-not-exist')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /text/)
-      .expect(404, done);
+describe('Server', () => {
+  describe('Not Found Handler', () => {
+    it('should respond with a not found message', (done) => {
+      request(server)
+        .get('/this-does-not-exist')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /text/)
+        .expect(404, done);
+    });
   });
-});
 
-describe('GET /', () => {
-  it('should respond with a JSON message', (done) => {
-    request(server)
-      .get('/')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, {
-        message: '👋🌎🌍🌏 - Hello World!',
-      }, done);
+  describe('GET /', () => {
+    it('should respond with a JSON message', (done) => {
+      request(server)
+        .get('/')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, {
+          message: '👋🌎🌍🌏 - Hello World!',
+        }, done);
+    });
   });
 });
