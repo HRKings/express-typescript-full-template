b78f6f9148127dd68b258eccc259a4e914f29b58
Helton Reis <hrkings@rhaizzing.com>
2021-10-27 20:27:13 -0300

    [fix] Make environment utilities behave like functions

--- src/utils/Utilities.ts
+++ src/utils/Utilities.ts
@@ -1,2 +1,10 @@
-export const isProductionEnvironment = process.env.NODE_ENV === 'production';
-export const isDevelopmentEnvironment = process.env.NODE_ENV === 'development';
+export const isProductionEnvironment = () => process.env.NODE_ENV === 'production';
+export const isDevelopmentEnvironment = () => process.env.NODE_ENV === 'development';
+export const isTestingEnvironment = () => process.env.NODE_ENV === 'testing';
+
+export const defaultDatabasePorts: Record<string, number> = {
+  postgres: 5432,
+  mysql: 3306,
+  mariadb: 3306,
+  mssql: 1433,
+};
