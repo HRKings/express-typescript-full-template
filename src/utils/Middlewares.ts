e4803a7b78809d3fc3f5dc1fac9cd25fce0fcf63
Helton Reis <hrkings@rhaizzing.com>
2021-10-27 20:12:01 -0300

    [fix] Add `next` parameter to ErrorHandler

--- src/utils/Middlewares.ts
+++ src/utils/Middlewares.ts
@@ -1,19 +1,22 @@
-import { Request, Response } from 'express';
+/* eslint-disable no-unused-vars */
+
+import { NextFunction, Request, Response } from 'express';
 
 import { isProductionEnvironment } from './Utilities.js';
 
-export function NotFoundHandler(request: Request, response: Response, next: any) {
+export function NotFoundHandler(request: Request, response: Response, next: NextFunction) {
   response.status(404);
   const error = new Error(`🔍 - Not Found - ${request.originalUrl}`);
+
   next(error);
 }
 
-export function ErrorHandler(error: Error, request: Request, response: Response) {
+export function ErrorHandler(error: Error, request: Request, response: Response, next: NextFunction) {
   const statusCode = response.statusCode !== 200 ? response.statusCode : 500;
 
   response.status(statusCode);
   response.json({
     message: error.message,
-    stack: isProductionEnvironment ? '🥞 Nothing to see here 🥞' : error.stack,
+    stack: isProductionEnvironment() ? '🥞 Nothing to see here 🥞' : error.stack,
   });
 }
